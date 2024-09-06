314ee5d19cb8e33e1b1e082df8804d11522b6e66
nafshan223 <shanafshan223@gmail.com>
2024-09-06 19:41:58 +0530

    changs

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express= require("express")
-const cors=require('cors')
 const envv= require("dotenv")
 const connection = require("./config/mongoose")
 const router = require("./Router/testRouter")
@@ -8,11 +7,10 @@ const app=express()
 connection()
 app.use(express.json())
 const cors = require('cors');
-app.use(cors({ origin: 'https://effervescent-yeot-e694e1.netlify.app/',methods: 'GET,POST,PUT,DELETE' }));
 
 app.use("/",router)
 envv.config()
 
-const PORT=  4003
+const PORT= process.env.PORT || 4000
 
 app.listen(PORT,console.log(`running in ${PORT} `))
