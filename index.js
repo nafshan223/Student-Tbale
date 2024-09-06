69e6e6db549f537a0b4d25d1ca1128f3669185a6
nafshan223 <shanafshan223@gmail.com>
2024-09-06 19:38:08 +0530

    changs

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const router = require("./Router/testRouter")
 const app=express()
 connection()
 app.use(express.json())
+const cors = require('cors');
+app.use(cors({ origin: 'https://effervescent-yeot-e694e1.netlify.app/',methods: 'GET,POST,PUT,DELETE' }));
 
 app.use("/",router)
 envv.config()
 
-const PORT=  4001
+const PORT=  4003
 
 app.listen(PORT,console.log(`running in ${PORT} `))
